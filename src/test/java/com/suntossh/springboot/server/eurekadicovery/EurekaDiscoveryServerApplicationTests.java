18f2e0c504fa85c9fcb83b79ce0e79e180fc74b3
Suntossh <suntossh.numerouno@gmail.com>
2019-11-24 23:39:52 -0500

    First Eureka Discovery Server commit

---
+++ src/test/java/com/suntossh/springboot/server/eurekadicovery/EurekaDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suntossh.springboot.server.eurekadicovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
